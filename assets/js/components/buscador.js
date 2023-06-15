a1d1494790e785b5c5f14630a05422d8cb2b884e
Jesus Perez <nanoperezsanchez@gmail.com>
2023-06-15 00:21:13 -0400

    mi primer proyecto

---
+++ assets/js/components/buscador.js
@@ -0,0 +1,21 @@
+// const boxSearch = document.querySelector("#buscador")
+
+// boxSearch.addEventListener("keyup", () =>{
+//     let searchValue = boxSearch.value;
+//     let view = ``
+//     for (const item of stock) {
+//         if(item.title.toLowerCase().includes(searchValue)){
+//            view = `<article>
+//             <img src= "${item.img}" alt="${item.title}">
+//             <h3>${item.title}</h3>
+//             <p>${item.price}$</p>
+//             <button onclick="addToCart(${item.id})">agregar al carrito de compra</button>
+//             <details>
+//                 <summary>Descripcion</summary>
+//                 <p>${item.desc}</p>
+//             </details>
+//             </article>`
+//         }
+//     }
+//     productsDOM.innerHTML = view
+// });
---
+++ assets/js/stock.js
@@ -0,0 +1,37 @@
+const stock = [
+    {
+        title: "Revolution",
+        price:72.95,
+        desc: "son zapatos excelentes para mujer",
+        id:1,
+        img: "https://cdn.wallapop.com/images/10420/eg/e4/__/c10420p874059457/i3134371879.jpg?pictureSize=W320"
+    },
+    {
+        title: "Jordan",
+        price: 295,
+        desc: "son zapatos excelentes para deporte",
+        id:2,
+        img: "https://media.revistagq.com/photos/62a314534741781ec3a0ac45/master/pass/chris-henry-tV8yaU09t7w-unsplash.jpg"
+    },
+    {
+        title: "gimnasio",
+        price: 150,
+        desc: "son zapatillas para hacer ejercicio",
+        id:3,
+        img:"https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b6gpll6zin5i8ayra255/air-monarch-iv-zapatillas-de-entrenamiento-t6VLxN.png"
+    },
+    {
+        title:"shifu",
+        price:225,
+        desc: "zapatos pefectos para caballero",
+        id:4,
+        img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c1398db2-8503-4a45-803a-ab50dd84ddaa/air-max-sc-zapatillas-z1CqsQ.png"
+    },
+    {
+        title:"fushi",
+        price:150,
+        desc: "zapatos pefectos para casual",
+        id:5,
+        img: "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/e6da41fa-1be4-4ce5-b89c-22be4f1f02d4/air-force-1-07-zapatillas-GjGXSP.png"
+    },   
+];
